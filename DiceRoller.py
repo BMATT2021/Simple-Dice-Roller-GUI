a30f96d77808c3dea065e161db98ab3948bde721
BMATT2021 <64337663+BMATT2021@users.noreply.github.com>
2022-12-08 06:32:04 -0600

    Update DiceRoller.py

--- DiceRoller.py
+++ DiceRoller.py
@@ -15,6 +15,9 @@ def roll1():
 def roll2():
     dice2 = random.randint(1, 20)
     label2["text"] = str(dice2)
+def roll3():
+    dice3 = random.randint(1,12)
+    label3["text"] = str(dice3)
 
     
 root = Tk()
@@ -25,12 +28,21 @@ label1.grid(row=0, column=1, sticky=N+S+E+W)
 
 label2 = Label(root, text="0", font=("Comic Sans", 112))
 label2.grid(row=2, column=1, sticky=N+S+E+W)
+
+label3 = Label(root, text="0", font=("Comic Sans", 112))
+label3.grid(row=0, column=3, sticky=N+S+E+W)
+
+button3 = Button(root, text="Roll D12", command=roll3)
+button3.grid(row=1, column=3, columnspan=2, sticky=N+S+E+W)
+
 button2 = Button(root, text="Roll D20", command=roll2)
 button2.grid(row=3, column=0, columnspan=2, sticky=N+S+E+W)
+
 button = Button(root, text="Roll D6", command=roll1)
 button.grid(row=1, column=0, columnspan=2, sticky=N+S+E+W)
-T = Text(root, height=5, width = 52)
-T.grid(row=1,column=3, sticky=N+S+E+W)
+
+T = Text(root, height=5, width = 20,)
+T.grid(row=1,column=2, sticky=N+S+E+W)
 
 root.columnconfigure(0, weight=1)
 root.columnconfigure(1, weight=1)
